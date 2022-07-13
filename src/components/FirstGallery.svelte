fd787fef2adfed64ba9f9848784b9d861396495d
thedivtagguy <amanbhargava2001@gmail.com>
2022-07-13 16:41:29 +0530

    Add Divider

--- src/components/FirstGallery.svelte
+++ src/components/FirstGallery.svelte
@@ -1,5 +1,21 @@
-<section class="px-8 bg-[color:#F5EBE0] h-1/2    w-full mx-auto text-gray-600 body-font ">
-    <div class="container justify-items-center w-2/3 items-center px-5 py-24 mx-auto flex">
+
+<main class="flex px-8 bg-[color:#F5EBE0] h-1/2 justify-between items-center    w-full mx-auto text-gray-600 body-font">
+
+<!-- Section 1 -->
+<section class="py-20 px-20 ">
+  <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+      <h2 class="text-3xl font-cormorant font-bold tracking-tight text-gray-900 sm:text-4xl md:text-7xl xl:text-7xl">
+         A Nice Headline Fits Here
+      </h2>
+      <p class="max-w-md mx-auto mt-3 text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+          Use our award-winning tools to help you maximize your profits. We've uncovered the correct recipe for converting visitors into customers.
+      </p>
+     
+  </div>
+</section>
+
+  <section class="px-8 bg-[color:#F5EBE0] h-1/2    w-full mx-auto text-gray-600 body-font ">
+    <div class="justify-items-center w-4/5 items-center px-5 py-24 mx-auto flex">
       <div class="flex justify-items-center items-center  md:-m-2 -m-1">
         <div class="flex justify-around -mt-[160px]  items-center flex-wrap ">
           <div class="md:p-2 p-1 w-1/2">
@@ -15,3 +31,4 @@
       </div>
     </div>
   </section>
+</main>
