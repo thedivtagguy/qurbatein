fcd8d3ff4100603c6fc31796069cd31370e75318
thedivtagguy <amanbhargava2001@gmail.com>
2022-07-25 17:49:41 +0530

    Fix Photo Essay Grid

--- src/components/PhotoEssay.svelte
+++ src/components/PhotoEssay.svelte
@@ -8,11 +8,11 @@
   
 <!-- Section 1 -->
 <section class="relative bg-[color:#e3d5ca] w-full">
-    <div class="relative w-full  px-5 py-10 mx-auto sm:py-12 md:py-16  max-w-7xl">
+    <div class="relative w-full   py-10 mx-auto sm:py-12 md:py-16  max-w-7xl">
 
-        <h2 class="font-cormorant  font-bold  leading-10 tracking-tight text-left text-[color:var(--color-text)] md:text-left sm:leading-none text-6xl md:text-6xl lg:text-[6rem]">Photo Essays</h2>
-        <div class="flex grid h-full grid-cols-12 gap-10 pb-10 mt-8 ">
-            <div class="relative shadow-xl  flex flex-col items-start justify-end h-full col-span-10 overflow-hidden rounded-xl group md:col-span-6 xl:col-span-4">
+        <h2 class="font-cormorant  font-bold  leading-10 tracking-tight text-left text-[color:var(--color-text)] md:text-left sm:leading-none text-6xl md:text-6xl px-8 lg:text-[6rem]">Photo Essays</h2>
+        <div class="flex grid h-full grid-cols-1 px-8 lg:grid-cols-12 gap-10 pb-10 mt-8 ">
+            <div class="relative shadow-xl  flex flex-col items-start justify-end h-full  overflow-hidden rounded-xl group md:col-span-6 xl:col-span-4">
                 <a href="/photo-essay/{photoEssay[0].slug}" class="block w-full transition duration-300 ease-in-out transform bg-center bg-cover h-[420px] hover:scale-110" style="background-image:url('/images/call-1.jpg'); filter:sepia(0.5);">
                 </a>
                 <div class="relative z-20 w-full h-[180px] py-8 text-[color:var(--color-text)] bg-[color:#d6c2b3] border-t-0 border-yellow-200 px-7">
@@ -21,7 +21,7 @@
 
                 </div>
             </div>
-            <div class="relative shadow-xl flex flex-col items-start justify-end h-full col-span-10 overflow-hidden rounded-xl group md:col-span-6 xl:col-span-4">
+            <div class="relative shadow-xl flex flex-col items-start justify-end h-full  overflow-hidden rounded-xl group md:col-span-6 xl:col-span-4">
                 <a href="/photo-essay/{photoEssay[1].slug}" class="block w-full transition duration-300 ease-in-out transform bg-center bg-cover h-[420px] hover:scale-110" style="background-image:url('/images/call-4.jpg'); filter:sepia(0.5);">
                 </a>
                 <div class="relative z-20 w-full h-[180px] py-8 text-[color:var(--color-text)] bg-[color:#d6c2b3] border-t-0 border-yellow-200 px-7">
@@ -31,7 +31,7 @@
                 </div>
             </div>
 
-            <div class="relative shadow-xl flex flex-col items-start justify-end h-full col-span-10 overflow-hidden rounded-xl group md:col-span-6 xl:col-span-4">
+            <div class="relative shadow-xl flex flex-col items-start justify-end h-full  overflow-hidden rounded-xl group md:col-span-6 xl:col-span-4">
                 <a href="/photo-essay/{photoEssay[2].slug}" class="block w-full transition duration-300 ease-in-out transform bg-center bg-cover h-[420px] hover:scale-110" style="background-image:url('/images/paradox-1.jpg'); filter:sepia(0.5);">
                 </a>
                 <div class="relative z-20 w-full h-[180px] py-8 text-[color:var(--color-text)] bg-[color:#d6c2b3] border-t-0 border-yellow-200 px-7">
