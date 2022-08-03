a950e1c90dfd4117ed6e5af82f1e0ae911e52dc0
thedivtagguy <amanbhargava2001@gmail.com>
2022-08-03 22:37:07 +0530

    Fix Headings Seductions

--- src/routes/articles/[article].svelte
+++ src/routes/articles/[article].svelte
@@ -76,7 +76,7 @@
 
 
         {#if block.Type == "h2"}
-        <section class="lg:w-2/3 py-2 mx-auto">
+        <section class="py-4 mx-auto">
     
          <h2> {block.Text}</h2>
         </section>
